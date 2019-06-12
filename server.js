a8ee5635c1450fd31d77970cadaa28aa1c90b8e5
TheMitri4 <DimaMK98@gmail.com>
2019-06-12 17:15:36 +0300

    heroku-setup

--- server.js
+++ server.js
@@ -37,6 +37,8 @@ app.get('/fight-page', function(req, res){
 	})
 });
 
-app.listen(3000, '192.168.0.102', function(){
-	console.log('Server is running on port 3000');
+const port = process.env.PORT || 3000;
+
+app.listen(port, function(){
+	console.log('Server is running on port ' + port);
 });
